a2ddd5c06098f946aedc233651fe7607b707881d
andri <andri@fabrika.al>
2015-07-03 18:53:02 +0200

    Finalize the way of determining whether a path is a directory

--- Rotor/Path.php
+++ Rotor/Path.php
@@ -1,89 +1,102 @@
 <?php
 namespace Rotor;
 
-class Path {
+class Path
+{
 
-	protected $pathStr = '';
-	protected $is_dir = false;
+    protected $pathStr = '';
+    protected $is_dir = false;
+    protected $is_absolute = false;
 
-	protected static $dirCache = [];
+    protected static $dirCache = [];
 
-	/**
-	 * @param string $pathStr
-	 * @return Path
-	 */
-	public static function Create($pathStr) {
-		return new Path($pathStr);
-	}
+    /**
+     * @param string $pathStr
+     * @return Path
+     */
+    public static function Create($pathStr)
+    {
+        return new Path($pathStr);
+    }
 
-	/**
-	 * @param string $pathStr
-	 */
-	public function __construct($pathStr) {
-		$this->pathStr = $this->compactPath($pathStr);
-		$this->is_dir = $this->detectDirectoryStatus();
-	}
+    public static function CreateDir($pathStr)
+    {
+        return new Path($pathStr, true);
+    }
 
-	protected function compactPath($pathStr){
-		$hasLeadingSlash = substr($pathStr,0,1) == '/';
-		$hasTrailingSlash = mb_substr($pathStr,-1) == '/';
-		$parts = explode('/',$pathStr);
+    /**
+     * @param string $pathStr
+     * @param bool $as_directory
+     */
+    public function __construct($pathStr, $as_directory = false)
+    {
+        $this->pathStr = $this->compactPath($pathStr);
+        $this->is_dir = $as_directory || $this->hasTrailingSlash();
+    }
 
-		$resultParts = [];
-		for ($i = 0; $i < count($parts); $i++) {
-			if ($parts[$i] != '' && $parts[$i] != '.'){
-				if ($parts[$i] == '..'){
-					if (count($resultParts)>0){
-						array_pop($resultParts);
-					}
-				} else {
-					$resultParts[] = $parts[$i];
-				}
-			}
-		}
+    protected function compactPath($pathStr)
+    {
+        $hasLeadingSlash = substr($pathStr, 0, 1) == '/';
+        $hasTrailingSlash = mb_substr($pathStr, -1) == '/';
+        $parts = explode('/', $pathStr);
 
-		$result = implode('/',$resultParts);
-		if ($hasLeadingSlash) {
-			$result = '/'.$result;
-		}
-		if ($hasTrailingSlash and $result != '/') {
-			$result.='/';
-		}
-		return $result;
-	}
+        $resultParts = [];
+        for ($i = 0; $i < count($parts); $i++) {
+            if ($parts[$i] != '' && $parts[$i] != '.') {
+                if ($parts[$i] == '..') {
+                    if (count($resultParts) > 0) {
+                        array_pop($resultParts);
+                    }
+                } else {
+                    $resultParts[] = $parts[$i];
+                }
+            }
+        }
 
-	protected function detectDirectoryStatus(){
-		if (mb_substr($this->pathStr,-1) == '/') {
+        $result = implode('/', $resultParts);
+        if ($hasLeadingSlash) {
+            $result = '/' . $result;
+        }
+        if ($hasTrailingSlash and $result != '/') {
+            $result .= '/';
+        }
+        return $result;
+    }
 
-		}
-	}
+    protected function hasTrailingSlash()
+    {
+        return (mb_substr($this->pathStr, -1) == '/');
+    }
 
-	public function isDirectory() {
-		return $this->is_dir;
-	}
+    public function isDirectory()
+    {
+        return $this->is_dir;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function __toString(){
-		return $this->pathStr;
-	}
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->pathStr;
+    }
 
-	/**
-	 * @param str $str
-	 * @return Path
-	 */
-	public function append($str) {
-		$currPath = $this->pathStr;
-		if (substr($currPath,-1) == '/') {
-			$currPath = substr($currPath, 0, mb_strlen($currPath) - 1);
-		}
-		if (substr($str,0,1) == '/') {
-			$str = substr($str,1);
-		}
-		$resultStr = $currPath.'/'.$str;
+    /**
+     * @param str $str
+     * @return Path
+     */
+    public function append($str)
+    {
+        $currPath = $this->pathStr;
+        if (substr($currPath, -1) == '/') {
+            $currPath = substr($currPath, 0, mb_strlen($currPath) - 1);
+        }
+        if (substr($str, 0, 1) == '/') {
+            $str = substr($str, 1);
+        }
+        $resultStr = $currPath . '/' . $str;
 
-		return new Path($resultStr);
-	}
+        return new Path($resultStr);
+    }
 
 }
--- Test/PathTest.php
+++ Test/PathTest.php
@@ -7,66 +7,82 @@
  */
 
 namespace Rotor\Test;
+
 use Rotor\Path;
 
-class PathTest extends \PHPUnit_Framework_TestCase {
+class PathTest extends \PHPUnit_Framework_TestCase
+{
 
-	/**
-	 * @test
-	 */
-	public function creates_a_Path_object_via_static_call(){
-		$path = Path::Create('');
-		$this->assertInstanceOf('Rotor\Path',$path);
-	}
+    /**
+     * @test
+     */
+    public function creates_a_Path_object_via_static_call()
+    {
+        $path = Path::Create('');
+        $this->assertInstanceOf('Rotor\Path', $path);
+    }
 
-	/**
-	 * @test
-	 */
-	public function creates_a_Path_object_via_constructor(){
-		$path = new Path('');
-		$this->assertInstanceOf('Rotor\Path',$path);
-	}
+    /**
+     * @test
+     */
+    public function creates_a_Path_object_via_constructor()
+    {
+        $path = new Path('');
+        $this->assertInstanceOf('Rotor\Path', $path);
+    }
 
-	///**
-	// * @test
-	// */
-	//public function detects_a_trailing_slash_as_a_directory(){
-	//
-	//}
+    /**
+     * @test
+     */
+    public function detects_a_trailing_slash_as_a_directory(){
+        $path = new Path('/');
+        $this->assertTrue($path->isDirectory());
 
-	/**
-	 * test
-	 */
-	public function compacts_a_path_with_multiple_consecutive_slashes(){
-		$path = new Path('/path//to/somewhere/');
-		$this->assertEquals('/path/to/somewhere/',$path->__toString());
+        $path = new Path('/var/test/');
+        $this->assertTrue($path->isDirectory());
 
-		$path = new Path('/path//to////somewhere/');
-		$this->assertEquals('/path/to/somewhere/',$path->__toString());
+        $path = new Path('notAblsolute/test/');
+        $this->assertTrue($path->isDirectory());
+    }
 
-		$path = new Path('///');
-		$this->assertEquals('/',$path->__toString());
-	}
+    /**
+     * test
+     */
+    public function interprets_lack_of_trailing_slash_as_not_a_directory()
+    {
+        $path = new Path('/var/test');
+        $this->assertFalse($path->isDirectory());
+    }
 
-	/**
-	 * @test
-	 */
-	public function functions_as_realpath_but_with_non_existing_paths(){
-		$path = new path('/var/../test/');
-		$this->assertEquals('/test/',$path->__toString());
+    /**
+     * test
+     */
+    public function compacts_a_path_with_multiple_consecutive_slashes()
+    {
+        $path = new Path('/path//to/somewhere/');
+        $this->assertEquals('/path/to/somewhere/', $path->__toString());
 
-		$path = new path('/var/./test/');
-		$this->assertEquals('/var/test/',$path->__toString());
+        $path = new Path('/path//to////somewhere/');
+        $this->assertEquals('/path/to/somewhere/', $path->__toString());
 
-		$path = new path('/var/./test/.././././../');
-		$this->assertEquals('/',$path->__toString());
-	}
+        $path = new Path('///');
+        $this->assertEquals('/', $path->__toString());
+    }
 
+    /**
+     * @test
+     */
+    public function functions_as_realpath_but_with_non_existing_paths()
+    {
+        $path = new path('/var/../test/');
+        $this->assertEquals('/test/', $path->__toString());
 
+        $path = new path('/var/./test/');
+        $this->assertEquals('/var/test/', $path->__toString());
 
-
-
-
+        $path = new path('/var/./test/.././././../');
+        $this->assertEquals('/', $path->__toString());
+    }
 
 
 }
